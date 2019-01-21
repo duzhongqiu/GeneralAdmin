1a121d579dda00824e1091fe8e00a0817cb93a36
zqd <573293679@qq.com>
2019-01-21 17:41:28 +0800

    前端

--- blogs/admin.py
+++ blogs/admin.py
@@ -13,14 +13,14 @@ class TagAdmin(admin.ModelAdmin):
     list_filter = ('name',)
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title','category','content','pub')
+    list_display = ('title','author','category','标签','pub')
     search_fields = ('title',)
-    list_filter = ('title','category','content','pub')
+    list_filter = ('category','tag','pub')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('blog','name','content','pub')
-    search_fields = ('name',)
-    list_filter = ('blog','name','content','pub')
+    search_fields = ('blog',)
+    list_filter = ('name','pub')
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Tag,TagAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -7,10 +7,11 @@ class Category(models.Model):
     def  __str__(self):
         return self.name
     class Meta:
-        verbose_name='类别'
-        verbose_name_plural = '类别'
+        verbose_name='栏目'
+        verbose_name_plural = '栏目'
 
 class Tag(models.Model):
+    #博客的标签
     name = models.CharField('名称',max_length=30)
     def  __str__(self):
         return self.name
@@ -19,14 +20,16 @@ class Tag(models.Model):
         verbose_name_plural = '标签'
 
 class Blog(models.Model):
+    #博客的内容
     title = models.CharField('标题',max_length=32)
     author = models.CharField('作者', max_length=16)
     content = RichTextUploadingField('内容',null=True, blank=True)
     pub = models.DateField('发布时间',auto_now_add=True)
-    category = models.ForeignKey(Category,verbose_name='分类',on_delete=models.CASCADE)#多对一（博客-类别）
+    category = models.ForeignKey(Category,verbose_name='栏目',on_delete=models.CASCADE)#多对一（博客-类别）
     tag = models.ManyToManyField(Tag,verbose_name='标签')
 
-
+    def 标签(self):
+        return [tag.name for tag in self.tag.all()]
     def __str__(self):
         return self.title
     class Meta:
@@ -34,11 +37,12 @@ class Blog(models.Model):
         verbose_name_plural = '博客'
 
 class Comment(models.Model):
+    #评论
     blog = models.ForeignKey(Blog,verbose_name='博客',on_delete=models.CASCADE)
-    name = models.CharField('称呼',max_length=16)
+    name = models.CharField('用户',max_length=16)
     email = models.EmailField('邮箱')
-    content = models.CharField('内容',max_length=240)
-    pub = models.DateField('发布时间',auto_now_add=True)
+    content = models.CharField('内容',max_length=200)
+    pub = models.DateField('评论时间',auto_now_add=True)
     def __str__(self):
         return self.content
     class Meta:
